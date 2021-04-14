be3902872eaa574dda51f26acac6f69619a91eb7
jericho <baofeng.qian@maitao.com>
2021-04-14 16:45:25 +0800

    fix(master):

---
+++ Sources/Base/JXLabel.h
@@ -0,0 +1,16 @@
+//
+//  JXLabel.h
+//  JXCategoryView
+//
+//  Created by blackstone jericho on 2021/4/14.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JXLabel : UILabel
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Sources/Base/JXLabel.m
@@ -0,0 +1,20 @@
+//
+//  JXLabel.m
+//  JXCategoryView
+//
+//  Created by blackstone jericho on 2021/4/14.
+//
+
+#import "JXLabel.h"
+
+@implementation JXLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
